9120b2774ed1fdcaeba4546dd1567b565680aa44
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-12-11 23:47:51 -0500

    feat: Add streaming capability to main.py
    - Added the `stream_request` function to handle streaming responses
    - Created a `main` function to execute the script and handle streaming requests
    - Updated the `endpoint` data to include `"stream": True` for streaming requests
    
    These changes enhance the script's functionality, allowing it to handle streaming interactions with the local server.

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from typing import List, Literal, TypedDict
+import sys
+import time
+from typing import List
 
 import requests
 
-
-class ChatCompletion(TypedDict):
-    role: Literal["system", "assistant", "function", "user"]
-    content: str
+from llama_cpp_client.types import ChatMessage
 
 
 def make_request(endpoint, method="POST", data=None):
@@ -28,21 +27,56 @@ def make_request(endpoint, method="POST", data=None):
     return response.json()
 
 
-def get_prompt_sequence(messages: List[ChatCompletion]) -> str:
+def stream_request(endpoint, method="POST", data=None):
     """
-    Converts a sequence of chat messages into a single formatted string.
+    Send a request to the local server.
+
+    :param endpoint: The API endpoint to send the request to.
+    :param method: HTTP method to use (default is 'POST').
+    :param data: Data to be sent with the request, expected to be a dictionary.
+    :return: The server's response.
+    """
+    # >>> response = requests.post(url, json=data, headers=headers, stream=True)
+    # >>> lines = response.iter_lines()
+    # >>> for line in lines:
+    # ...     print(line)
+    # ...
+    # b'data: {"content":"Hello","multimodal":false,"slot_id":0,"stop":false}'
+    # b''
+    # b'data: {"content":" Austin","multimodal":false,"slot_id":0,"stop":false}'
+    # b''
+    # b'data: {"content":"!","multimodal":false,"slot_id":0,"stop":false}'
+    # b''
+    url = f"http://127.0.0.1:8080{endpoint}"
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.post(url, json=data, headers=headers, stream=True)
+
+    for line in response.iter_lines():
+        if line:
+            chunk = line[len("data: ") :]
+            print(chunk, end=None)
+            sys.stdout.flush()
+    print()
+
+    return response.json()
+
+
+def get_prompt_sequence(messages: List[ChatMessage]) -> str:
+    """
+    Converts a sequence of chat messages into a single formatted string, omitting the role.
 
     :param messages: A list of chat messages with roles and content.
-    :return: A formatted string representing the conversation.
+    :return: A formatted string representing the conversation content.
     """
-    return "".join([f"{msg['role']}: {msg['content']}" for msg in messages])
+    return "".join([msg["content"] for msg in messages])
 
 
-system_message = ChatCompletion(
+system_message = ChatMessage(
     role="system",
-    content="<<SYS>>My name is Mistral and I am a helpful assistant<</SYS>>\n",
+    content="<<SYS>>My name is Mistral and I am a helpful assistant.<</SYS>>\n",
 )
-user_message = ChatCompletion(
+user_message = ChatMessage(
     role="user",
     content="[INST] Hello! My name is Austin. What is your name? [/INST]\n",
 )
@@ -53,7 +87,13 @@ prompt = get_prompt_sequence(messages)
 
 # Example usage:
 endpoint = "/completion"
-data = {"prompt": prompt}
+data = {"prompt": prompt, "stream": True}
 
-response = make_request(endpoint, data=data)
-print(response["content"])
+
+def main():
+    response = make_request(endpoint, data=data)
+    print(response["content"])
+
+
+if __name__ == "__main__":
+    main()
