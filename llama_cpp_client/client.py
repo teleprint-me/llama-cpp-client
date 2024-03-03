3f558abd026267ca3b4bda51d46a15b777bb1e3f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-03-02 20:13:41 -0500

    refactor: Apply chat completions endpoint to reduce complexity for client example

--- llama_cpp_client/client.py
+++ llama_cpp_client/client.py
@@ -1,6 +1,7 @@
 """
 Module: llama_cpp_client.client
 """
+
 import json
 import sys
 from typing import Any, Callable, List
@@ -13,12 +14,10 @@ from llama_cpp_client.request import LlamaCppRequest
 from llama_cpp_client.types import ChatMessage
 
 
-class LlamaCppAPI:
-    ...  # TODO
+class LlamaCppAPI: ...  # TODO
 
 
-class LlamaCppClient:
-    ...  # TODO
+class LlamaCppClient: ...  # TODO
 
 
 def get_current_weather(location: str, unit: str = "celsius"):
@@ -72,49 +71,56 @@ function_schemas = [
     }
 ]
 
-# NOTE: Mixtral is a Instruct based MoE Model which is composed of 8 7B Mistral models
-system_message = ChatMessage(
-    role="system",
-    content="<<SYS>>My name is Mistral. I am a kind and helpful assistant.<</SYS>>\n",
-)
-user_message = ChatMessage(
-    role="user",
-    content="[INST] Hello! My name is Austin. What is your name? [/INST]\n",
-)
-model_message = ChatMessage(
-    role="user",
-    content="Hello Austin, nice to meet you! I'm Mistral, a helpful assistant. How can I assist you today?",
-)
-user_message = ChatMessage(
-    role="user",
-    content="[INST] Can you show me a function definition for the fibonacci sequence? [/INST]\n",
-)
-messages = [system_message, user_message, model_message, user_message]
-
-# Example usage:
-endpoint = "/completion"
-
 
 def main():
     pretty.install()
-    llama_cpp_request = LlamaCppRequest("http://127.0.0.1", "8080")
+
+    # NOTE: Mixtral is a Instruct based MoE Model which is composed of 8 7B Mistral models
+    system_message = ChatMessage(
+        role="system",
+        content="My name is StableLM. I am a supportive and helpful assistant.\n",
+    )
+    user_message = ChatMessage(
+        role="user",
+        content="Hello! My name is Austin. What is your name?\n",
+    )
+    model_message = ChatMessage(
+        role="assistant",
+        content="Hello Austin, nice to meet you! I'm StableLM, a helpful assistant. How can I assist you today?",
+    )
+    user_message = ChatMessage(
+        role="user",
+        content="How can I get of a list of prime number in Python?\n",
+    )
+    messages = [system_message, user_message, model_message, user_message]
+
+    # Example usage:
+    # endpoint = "/completion"
+    base_url = "http://127.0.0.1"
+    port = "8080"
+    endpoint = "/v1/chat/completions"
+
+    llama_cpp_request = LlamaCppRequest(base_url=base_url, port=port)
     # `grammar`: Set grammar for grammar-based sampling (default: no grammar)
 
-    # Create the prompt using the get_prompt_sequence function
-    prompt = get_prompt_sequence(messages)
     # Structure the prompt for to stream the request
-    data = {"prompt": prompt, "stream": True}
+    data = {"messages": messages, "stream": True}
     # Generate the models response
     generator = llama_cpp_request.stream(endpoint, data=data)
     # Handle the models generated response
-    response = ""
-    for chunk in generator:
-        response += chunk["content"]  # append each chunk to the response
-        print(chunk["content"], end="")  # print the chunk out to the user
-        sys.stdout.flush()  # flush the output to standard output
+    content = ""
+    for response in generator:
+        if "content" in response["choices"][0]["delta"]:
+            # extract the token from the response
+            token = response["choices"][0]["delta"]["content"]
+            # append each chunk to the response
+            content += token
+            print(token, end="")  # print the chunk out to the user
+            sys.stdout.flush()  # flush the output to standard output
+
     print()  # add padding to models output
     # Build the models response
-    model_message = ChatMessage(role="user", content=response)
+    model_message = ChatMessage(role="user", content=content)
     messages.append(model_message)
 
 
