10f90082a6810a667a22e1c8dfcff59a6b8af0f1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-03-02 21:51:56 -0500

    chore: Update package metadata

--- llama_cpp_client/__init__.py
+++ llama_cpp_client/__init__.py
@@ -3,8 +3,8 @@ Module: llama_cpp_client
 """
 
 __version__ = "0.0.1"
-__name__ = "llama_cpp_client"
-__agent__ = None  # f"teleprint-me/{__name__}"
-__source__ = None  # f"https://github.com/{__agent__}"
+__name__ = "llama-cpp-client"
+__agent__ = f"teleprint-me/{__name__}"
+__source__ = f"https://github.com/{__agent__}"
 __author__ = "Austin Berrio"
 __email__ = "aberrio@teleprint.me"
