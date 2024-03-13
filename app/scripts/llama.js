eb86385212bf32466465dd27b6541a66ba3ff87e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-03-13 03:50:11 -0400

    refactor: Add completion methods
    - Add debugging console.log statements
    - Remove requestData parameter from callback when streaming responses
    - Add a optional default for instantiating the LlamaRequest object
    - Comment out setting regeneration and parameters button event listeners for LlamaClient
    - Add helper methods for toggling the generation button
    - Add a helper method for signaling an error state with completions
    - Refactor the handleGenerateCompletion method
    - Add the generateModelCompletion helper function
    - Simplify the LlamaClient implementation
    - Add wrapper for initializing app when DOM Content is Loaded.

--- app/scripts/llama.js
+++ app/scripts/llama.js
@@ -15,11 +15,13 @@ class LlamaRequest {
 
     // !IMPORTANT: GET requests cannot have a body!
     if (method === 'GET') {
+      console.log('GET', 'handle response', url);
       response = await fetch(url, {
         method: method,
         headers: headers
       });
     } else {
+      console.log('POST', 'handle response', url);
       response = await fetch(url, {
         method: method,
         headers: headers,
@@ -29,10 +31,10 @@ class LlamaRequest {
 
     if (!response.ok) {
       console.error(response.status, response.type, response.headers);
-
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return response.json();
+    console.log('response is ok');
+    return response; // NOTE: Returning json breaks streaming!
   }
 
   async get(endpoint, requestData) {
@@ -41,6 +43,7 @@ class LlamaRequest {
   }
 
   async post(endpoint, requestData) {
+    console.log('POST', endpoint, requestData);
     const url = `${this.baseUrl}:${this.port}${endpoint}`;
     return await this._handleResponse(url, 'POST', requestData);
   }
@@ -58,6 +61,7 @@ class LlamaRequest {
    * @returns {Object} The parsed JSON object from the server response.
    */
   _processStreamValue(value) {
+    console.log('Processing stream value', value);
     // Decode the streamed value into a string
     const tokenString = new TextDecoder('utf-8').decode(value);
     // Split the string on "data: " to handle concatenated responses
@@ -66,16 +70,21 @@ class LlamaRequest {
     const potentialJson = split[split.length - 1];
     // Parse the potential JSON object
     const token = JSON.parse(potentialJson);
+    console.log('Parsed JSON', token);
     // Return extracted chunked response
     return token;
   }
 
   async stream(endpoint, requestData, callback) {
+    console.log('starting stream...');
+    // response is a promise
     const response = await this.post(endpoint, requestData);
+    console.log('getting reader...');
     const reader = response.body.getReader();
     let endStream = false;
 
     while (true) {
+      console.log('processing stream for', endpoint);
       const { done, value } = await reader.read();
       if (done) {
         console.warn('Stream ended without a stop token.');
@@ -83,7 +92,7 @@ class LlamaRequest {
       }
 
       const token = this._processStreamValue(value);
-      endStream = callback(token, requestData);
+      endStream = callback(token);
 
       if (endStream) {
         console.log(
@@ -158,19 +167,24 @@ class LlamaCompletions {
   constructor(llamaRequest = null, parameters = null) {
     this.completions = []; // track raw completions
 
-    this.client = new LlamaAPI(llamaRequest || new LlamaRequest(), parameters);
+    this.llamaAPI = new LlamaAPI(
+      llamaRequest || new LlamaRequest(),
+      parameters
+    );
 
     // context window has formatted completions
     this.contextWindow = document.querySelector('div#context-window');
-
+    // text area has user input
     this.userPrompt = document.querySelector('textarea#user-prompt');
 
+    // buttons trigger events based on user input
     this.generateButton = document.querySelector('button#generate-completion');
     this.regenerateButton = document.querySelector(
       'button#regenerate-completion'
     );
     this.parametersButton = document.querySelector('button#model-parameters');
 
+    // setup event listeners
     this.setupListeners();
   }
 
@@ -178,16 +192,16 @@ class LlamaCompletions {
     // Add event listener to the generate button
     this.generateButton.addEventListener(
       'click',
-      this.handleGenerateCompletion.bind(this)
+      this.generateModelCompletion.bind(this)
     );
-    this.regenerateButton.addEventListener(
-      'click',
-      this.handleRegenerateCompletion.bind(this)
-    );
-    this.parametersButton.addEventListener(
-      'click',
-      this.handleModelParameters.bind(this)
-    );
+    // this.regenerateButton.addEventListener(
+    //   'click',
+    //   this.handleRegenerateCompletion.bind(this)
+    // );
+    // this.parametersButton.addEventListener(
+    //   'click',
+    //   this.handleModelParameters.bind(this)
+    // );
   }
 
   /**
@@ -213,32 +227,82 @@ class LlamaCompletions {
     return div; // this is added to contextWindow
   }
 
-  async handleGenerateCompletion(event) {
-    const prompt = this.userPrompt.value;
-    const completionDiv = this.createCompletion(prompt);
-
+  _toggleGenerateStop() {
     this.generateButton.querySelector('p').innerText = 'Stop';
     this.generateButton.querySelector('i').classList.remove('bx-play');
     this.generateButton.querySelector('i').classList.add('bx-stop');
+  }
+
+  _toggleGenerateStart() {
+    this.generateButton.querySelector('p').innerText = 'Generate';
+    this.generateButton.querySelector('i').classList.remove('bx-stop');
+    this.generateButton.querySelector('i').classList.add('bx-play');
+  }
+
+  _signalErrorState(completionDiv) {
+    completionDiv.classList.remove('animated-border'); // Stop the normal animation
+    completionDiv.classList.add('animated-border-error'); // Indicate an error state
+  }
+
+  async handleGenerateCompletion(completionDiv, prompt) {
+    console.log('using prompt to handle generating completion');
     try {
-      const promise = await this.client.getCompletions(
-        this.userPrompt.value, // prompt
-        function (token, requestData) {
-          // callback
-          return false; // because we're not doing anything yet
+      // Toggle the generate button
+      this._toggleGenerateStop();
+      // Request the model completion
+      const promise = await this.llamaAPI.getCompletions(
+        prompt, // prompt
+        // use lambda to avoid 'this' conflicts
+        function (token) {
+          // callback handles the models completion
+          if (token.stop) {
+            console.log(
+              `Received stop token. Predicted ${token.tokens_predicted} tokens.`
+            );
+            return true; // no content left to extract
+          }
+          console.log('got token', token.content);
+          prompt += token.content;
+          // Update the context window with the generated completion
+          completionDiv.innerHTML = marked.parse(prompt);
+          // Need to update to highlight all code blocks within the element
+          completionDiv.querySelectorAll('pre code').forEach((block) => {
+            hljs.highlightBlock(block);
+          });
+          MathJax.typesetPromise();
+          return false; // continue processing
         },
-        true // stream
+        true // enable streaming
       );
-      // Update the context window with the generated completion
+      this.completions.push(prompt);
     } catch (error) {
+      console.error('Failed to read stream:', error);
+      this._signalErrorState(completionDiv);
     } finally {
       // Ensure the animation is stopped regardless of how the loop exits
       completionDiv.classList.remove('animated-border');
       // restore original button widget in the ui
-      this.generateButton.querySelector('p').innerText = 'Generate';
-      this.generateButton.querySelector('i').classList.remove('bx-stop');
-      this.generateButton.querySelector('i').classList.add('bx-play');
+      this._toggleGenerateStart();
     }
+  }
+
+  generateModelCompletion(event) {
+    // Get the users input
+    const prompt = this.userPrompt.value.trim();
+    // Basic validation of user input
+    if (!prompt) {
+      alert('Please enter a prompt.');
+      return;
+    }
+    // Clear the input field
+    this.userPrompt.value = '';
+    // Create the assistants completions div with a loading state
+    const completionDiv = this.createCompletion(prompt);
+    // Add the completions div to the context window
+    this.contextWindow.appendChild(completionDiv);
+    MathJax.typesetPromise();
+    // Handle the streamed tokens and update the completion in real-time
+    this.handleGenerateCompletion(completionDiv, prompt);
   }
 }
 
@@ -247,48 +311,44 @@ class LlamaClient {
     this.name = 'llama-cpp-client';
     this.version = 'v1';
     this.state = 'prototype';
-    this.setup();
-  }
-
-  setup() {
-    this.request = new LlamaRequest();
-    this.client = new LlamaAPI(this.request);
     this.completions = new LlamaCompletions();
   }
 }
 
-// Initialize Llama client
-let llama = new LlamaClient();
-llama.setup();
-console.log(llama.name, llama.version, llama.state);
-console.log('Successfully initialized llama.js');
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize Llama client
+  let llama = new LlamaClient();
+  console.log('Successfully initialized llama.js');
+  console.log(llama.name, llama.version, llama.state);
 
-// Configure marked.js with highlight.js for code syntax highlighting
-marked.setOptions({
-  highlight: function (code, lang) {
-    // Function to handle code syntax highlighting using highlight.js
-    // Get the language if available, otherwise set it as plain text
-    const language = highlight.getLanguage(lang) ? lang : 'plaintext';
-    // Apply highlighting and get the highlighted code
-    return highlight.highlight(code, { language }).value;
-  },
-  // Use 'hljs' class prefix for compatibility with highlight.js CSS
-  langPrefix: 'hljs language-'
-});
-console.log('Successfully initialized marked.js');
+  // Configure marked.js with highlight.js for code syntax highlighting
+  marked.setOptions({
+    highlight: function (code, lang) {
+      // Function to handle code syntax highlighting using highlight.js
+      // Get the language if available, otherwise set it as plain text
+      const language = highlight.getLanguage(lang) ? lang : 'plaintext';
+      // Apply highlighting and get the highlighted code
+      return highlight.highlight(code, { language }).value;
+    },
+    // Use 'hljs' class prefix for compatibility with highlight.js CSS
+    langPrefix: 'hljs language-'
+  });
+  console.log('Successfully initialized marked.js');
 
-// Initialize MathJax for rendering math equations
-MathJax = {
-  tex: {
+  // Highlight all the code snippets in the document
+  hljs.highlightAll(); // Initial code highlighting, if any
+  console.log('Successfully initialized highlight.js');
+
+  // Initialize MathJax for rendering math equations
+  MathJax.tex = {
     inlineMath: [
       ['$', '$'], // Dollar signs for inline math
       ['\\(', '\\)'], // Parentheses for display math
       ['[', ']'] // Brackets for display math (alternative to parentheses)
     ]
-  }
-};
-console.log('Successfully initialized mathjax');
-
-// Highlight all the code snippets in the document
-hljs.highlightAll(); // Initial code highlighting, if any
-console.log('Successfully initialized highlight.js');
+  };
+  MathJax.svg = {
+    fontCache: 'global'
+  };
+  console.log('Successfully initialized mathjax');
+});
