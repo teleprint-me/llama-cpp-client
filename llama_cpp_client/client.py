891298a5128ff239a491a6b95a1819f999b62051
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-03-28 22:44:56 -0400

    feat: Add LlamaCppClient class definition
    - Remove LlamaCppAPI placeholder

--- llama_cpp_client/client.py
+++ llama_cpp_client/client.py
@@ -4,20 +4,124 @@ Module: llama_cpp_client.client
 
 import json
 import sys
-from typing import Any, Callable, List
+from typing import Any, Callable, Dict, List
 
 from rich import pretty, print
+from rich.console import Console
+from rich.live import Live
+from rich.markdown import Markdown
 
 from llama_cpp_client.format import get_prompt_sequence
 from llama_cpp_client.grammar import LlamaCppGrammar
+from llama_cpp_client.history import LlamaCppHistory
 from llama_cpp_client.request import LlamaCppRequest
+from llama_cpp_client.tokenizer import LlamaCppTokenizer
 from llama_cpp_client.types import ChatMessage
 
 
-class LlamaCppAPI: ...  # TODO
+class LlamaCppClient:
+    def __init__(
+        self,
+        request: LlamaCppRequest = None,
+        history: LlamaCppHistory = None,
+        tokenizer: LlamaCppTokenizer = None,
+        top_k: int = 50,
+        top_p: float = 0.90,
+        min_p: float = 0.1,
+        temperature: float = 0.7,
+        presence_penalty: float = 0.0,
+        frequency_penalty: float = 0.0,
+        repeat_penalty: float = 1.1,
+        n_predict: int = -1,
+        seed: int = 1337,
+        stream: bool = True,
+        cache_prompt: bool = True,
+    ) -> None:
+        # manage llama.cpp client instances
+        self.request = request
+        self.history = history
+        self.tokenizer = tokenizer
 
+        # set llama.cpp server payload
+        self.data = {
+            "messages": self.chat_history.messages,
+            # set model hyper parameters
+            "temperature": self.temperature,
+            "top_k": self.topk,
+            "top_p": self.top_p,
+            "n_predict": self.n_predict,
+            "seed": self.seed,
+            "repeat_penalty": self.repeat_penalty,
+            "min_p": self.min_p,
+            # set llama.cpp server flags
+            "stream": self.stream,
+            "cache_prompt": self.cache_prompt,
+        }
 
-class LlamaCppClient: ...  # TODO
+        self.console = Console()
+
+        self._render_messages_once_on_start()
+
+    def _render_messages_once_on_start(self) -> None:
+        self.chat_history.load()
+        for message in self.chat_history.messages:
+            self.console.print(
+                Markdown(message["content"]),
+            )
+
+    def encode(self, prompt: List[Dict[str, str]]) -> List[int]: ...
+
+    def decode(self, tokens: List[int]) -> List[Dict[str, str]]: ...
+
+    def health(self):
+        try:
+            return self.request.get("/health")
+        except Exception as e:
+            print(f"HealthError: {e}")
+
+    def slots(self):
+        try:
+            return self.request.get("/slots")
+        except Exception as e:
+            print(f"SlotsError: {e}")
+
+    def stream_completion(self, prompt):
+        block = "█ "
+        self.data.prompt = prompt
+        # Generate the models response
+        generator = llama_cpp_request.stream("/v1/completions", data=self.data)
+        with Live(console=self.console) as live:
+            content = ""  # concat model response chunks
+            for response in generator:
+                if "content" in response["choices"][0]["delta"]:
+                    content += response["choices"][0]["delta"]["content"]
+                if response["choices"][0]["finish_reason"] is not None:
+                    block = ""  # Clear the block
+                markdown = Markdown(content + block)
+                live.update(
+                    markdown,
+                    refresh=True,
+                )
+            print()  # add padding to models output
+
+        self.chat_history.append({"role": "assistant", "content": content})
+
+    def prompt_model(self):
+        """Feed structured input data for the language model to process."""
+        status = self.health()["status"]
+        assert status == "ok", "Server not ready or error!"
+        self.model_name = self.get_model_name()
+        while True:
+            try:
+                prompt = self.chat_history.prompt()
+                self.console.print(Markdown(prompt))
+                self.stream_completion(prompt=prompt)
+
+            # NOTE: Ctrl + c (keyboard) or Ctrl + d (eof) to exit
+            # Adding EOFError prevents an exception and gracefully exits.
+            except (KeyboardInterrupt, EOFError):
+                self.chat_history.save()
+                exit()
 
 
 def get_current_weather(location: str, unit: str = "celsius"):
