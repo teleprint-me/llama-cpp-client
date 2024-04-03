0547c9e4c00eb0c4fe0f1924ede1c0c0778e1cb9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-04-03 01:18:33 -0400

    refactor: Update variable names and fix param and arg references

--- llama_cpp_client/tokenizer.py
+++ llama_cpp_client/tokenizer.py
@@ -16,15 +16,15 @@ class LlamaCppTokenizer:
         """
         self.llama_cpp_request = request or LlamaCppRequest()
 
-    def tokenize(self, text: str) -> List[int]:
+    def tokenize(self, content: str) -> List[int]:
         """
         Tokenizes a given text using the server's tokenize endpoint.
 
-        :param text: The text to tokenize.
+        :param content: The text content to tokenize.
         :return: A list of token IDs.
         """
-        payload = {"content": text}
-        llama_cpp_response = self.llama_cpp_request.post("/tokenize", data=payload)
+        data = {"content": content}
+        llama_cpp_response = self.llama_cpp_request.post("/tokenize", data=data)
         return llama_cpp_response.get("tokens", [])
 
     def detokenize(self, tokens: List[int]) -> str:
@@ -34,8 +34,8 @@ class LlamaCppTokenizer:
         :param tokens: The list of token IDs to detokenize.
         :return: The detokenized text.
         """
-        payload = {"tokens": tokens}
-        llama_cpp_response = self.llama_cpp_request.post("/detokenize", data=payload)
+        data = {"tokens": tokens}
+        llama_cpp_response = self.llama_cpp_request.post("/detokenize", data=data)
         return llama_cpp_response.get("content", "")
 
 
@@ -44,10 +44,10 @@ if __name__ == "__main__":
     llama_cpp_request = LlamaCppRequest(base_url="http://127.0.0.1", port="8080")
 
     # Initialize the LlamaCppTokenizer instance
-    tokenizer = LlamaCppTokenizer(llama_cpp_request=llama_cpp_request)
+    tokenizer = LlamaCppTokenizer(request=llama_cpp_request)
 
     # Define the text to tokenize
-    text = "<|system|>\nMy name is StableLM. I am a helpful assistant.<|endoftext|>\n<|user|>\nHello! My name is Austin! What is your name?<|endoftext|>\n<|assistant|>\n"
+    text = "My name is Llama. I am a supportive and helpful assistant."
 
     # Tokenize the text
     tokens = tokenizer.tokenize(text)
