5b88691779c498fc96433f5e37f18c5a41bc814b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-04-23 18:10:17 -0400

    feat: Add completions support to LlamaCppClient API

--- llama_cpp_client/client.py
+++ llama_cpp_client/client.py
@@ -4,16 +4,14 @@ Module: llama_cpp_client.client
 
 import argparse
 import json
-from typing import Any, Callable, Dict, List
+import sys
+from typing import Any, Callable
 
 from rich import pretty, print
 from rich.console import Console
-from rich.live import Live
 from rich.markdown import Markdown
 
 from llama_cpp_client.api import LlamaCppAPI
-from llama_cpp_client.format import get_prompt_sequence
-from llama_cpp_client.grammar import LlamaCppGrammar
 from llama_cpp_client.history import LlamaCppHistory
 from llama_cpp_client.request import LlamaCppRequest
 from llama_cpp_client.tokenizer import LlamaCppTokenizer
@@ -38,85 +36,81 @@ class LlamaCppClient:
         self.tokenizer = tokenizer or LlamaCppTokenizer(self.api.request)
         self.console = Console()
 
-    @property
-    def cursor(self) -> str:
-        return "█ "
+    def _render_completions_once_on_start(self) -> None:
+        self.history.load()
+        element = ""
+        for completion in self.history.completions:
+            if completion["role"] == "prompt":
+                element = ""
+                element += completion["content"]
+            if completion["role"] == "completion":
+                element += completion["content"]
+            self.console.print(Markdown(f"**{completion['role']}**"))
+            self.console.print(element)
+            print()
 
     def _render_chat_completions_once_on_start(self) -> None:
         self.history.load()
-        for message in self.history.messages:
-            self.console.print(Markdown(f"**{message['role']}**"))
-            self.console.print(message["content"])
+        for completion in self.history.completions:
+            self.console.print(Markdown(f"**{completion['role']}**"))
+            self.console.print(completion["content"])
             print()
 
-    def _render_completions_once_on_start(self) -> None:
-        self.history.load()
-        for message in self.history.messages:
-            completion = ""
-            if message["role"] == "prompt":
-                completion += message["content"]
-            if message["role"] == "completion":
-                completion += message["content"]
-            self.console.print(completion)
-            print()
-
-    def stream_completion(self, prompt: str) -> None:
+    def stream_completion(self, prompt: str) -> str:
         content = ""
-        block = self.cursor
         # API only supports individual completions at the moment
         # Currently researching how to implement multi-prompting
         generator = self.api.completion(prompt)
-
+        self.console.print(self.history.completions[-1]["content"], end="")
         # Handle the model's generated response
-        with Live(console=self.console) as live:
-            for response in generator:
-                if "content" in response:
-                    content += response["content"]
-                if response["stop"]:
-                    block = ""  # Clear the block
-                live.update(content + block)
+        for response in generator:
+            if "content" in response:
+                content += response["content"]
+            self.console.print(response["content"], end="")
+            sys.stdout.flush()
         print()  # Pad model output
 
         return content
 
     def stream_chat_completion(self) -> str:
         content = ""
-        block = self.cursor
-        generator = self.api.chat_completion(self.history.messages)
+        generator = self.api.chat_completion(self.history.completions)
 
         print()  # Pad model output
         self.console.print(Markdown("**assistant**"))
-        with Live(console=self.console) as live:
-            for response in generator:
-                if "content" in response["choices"][0]["delta"]:
-                    content += response["choices"][0]["delta"]["content"]
-                if response["choices"][0]["finish_reason"] is not None:
-                    block = ""  # Clear the block
-                # markdown = Markdown(content + block)
-                live.update(content + block, refresh=True)
+        for response in generator:
+            if "content" in response["choices"][0]["delta"]:
+                content += response["choices"][0]["delta"]["content"]
+            # markdown = Markdown(content + block)
+            self.console.print(content, end="")
+            sys.stdout.flush()
         print()  # Pad model output
 
         return content
 
-    def run_prompt(self) -> None:
+    def run_completions(self) -> None:
         self._render_completions_once_on_start()
 
         while True:
             try:
+                self.console.print(Markdown("**prompt**"))
                 prompt = self.history.prompt()
                 self.history.append({"role": "prompt", "content": prompt})
-                self.stream_completion()
+                self.console.print(Markdown("**completion**"))
+                completion = self.stream_completion(prompt)
+                self.history.append({"role": "completion", "content": completion})
                 self.history.save()
             # NOTE: Ctrl + c (keyboard) or Ctrl + d (eof) to exit
             except KeyboardInterrupt:
-                message = self.history.pop()
-                print("Popped", message["role"], "message.")
+                if self.history.completions:
+                    completion = self.history.pop()
+                    print("Popped", completion["role"], "element from history.")
             # Adding EOFError prevents an exception and gracefully exits.
             except EOFError:
                 self.history.save()
                 exit()
 
-    def run_chat(self) -> None:
+    def run_chat_completions(self) -> None:
         """Feed structured input data for the language model to process."""
         self._render_chat_completions_once_on_start()
 
@@ -130,8 +124,8 @@ class LlamaCppClient:
                 self.history.save()
             # NOTE: Ctrl + c (keyboard) or Ctrl + d (eof) to exit
             except KeyboardInterrupt:
-                message = self.history.pop()
-                print("Popped", message["role"], "message.")
+                completion = self.history.pop()
+                print("Popped", completion["role"], "element from history.")
             # Adding EOFError prevents an exception and gracefully exits.
             except EOFError:
                 self.history.save()
@@ -156,12 +150,12 @@ def get_current_weather(location: str, unit: str = "celsius"):
 
 
 def get_function(
-    message: dict[str, str], functions: dict[str, Callable]
+    completion: dict[str, str], functions: dict[str, Callable]
 ) -> dict[str, Any]:
     # Note: the JSON response may not always be valid; be sure to handle errors
-    name = message["function_call"]["name"]
+    name = completion["function_call"]["name"]
     callback = functions[name]
-    arguments = json.loads(message["function_call"]["arguments"])
+    arguments = json.loads(completion["function_call"]["arguments"])
     content = callback(**arguments)
     response = {
         "role": "function",
@@ -195,32 +189,33 @@ def get_arguments() -> argparse.Namespace:
         description="A command-line interface for interacting with the llama language model."
     )
     parser.add_argument(
-        "-n",
         "--session-name",
         type=str,
         required=True,
         help="The caches session name",
     )
     parser.add_argument(
-        "-m",
         "--system-message",
         type=str,
         default="My name is Llama. I am a supportive and helpful assistant.",
         help="The language models system message",
     )
     parser.add_argument(
-        "-u",
         "--base-url",
         type=str,
         default="http://127.0.0.1",
         help="The servers url (default: http://127.0.0.1)",
     )
     parser.add_argument(
-        "-p",
         "--port",
         type=str,
         default="8080",
         help="The servers port (default: 8080)",
+    )
+    parser.add_argument(
+        "--completions",
+        action="store_true",
+        help="Run a completion with the language model (default: False; run chat completions)",
     )
     parser.add_argument(
         "--top-k",
@@ -273,7 +268,7 @@ def get_arguments() -> argparse.Namespace:
         "--stop",
         type=str,
         default="",
-        help="List of stop tokens to ignore (default: empty string; use comma delimited list without spaces).",
+        help="List of stop tokens to ignore (default: empty string; use comma delimited list, no spaces).",
     )
     return parser.parse_args()
 
@@ -298,13 +293,22 @@ def main():
         stop=stop,
     )
     llama_cpp_tokenizer = LlamaCppTokenizer(llama_cpp_request)
+
+    if args.completions:
+        args.system_message = None
     llama_cpp_history = LlamaCppHistory(args.session_name, args.system_message)
+
     llama_cpp_client = LlamaCppClient(
-        llama_cpp_api, llama_cpp_history, llama_cpp_tokenizer
+        llama_cpp_api,
+        llama_cpp_history,
+        llama_cpp_tokenizer,
     )
     # `grammar`: Set grammar for grammar-based sampling (default: no grammar)
 
-    llama_cpp_client.run_chat()
+    if args.completions:
+        llama_cpp_client.run_completions()
+    else:
+        llama_cpp_client.run_chat_completions()
 
 
 if __name__ == "__main__":
