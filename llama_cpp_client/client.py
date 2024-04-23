b6f13a0359cb0aaa98940fb7101b50dc9e227211
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-04-23 01:25:11 -0400

    feat: Add options for managing hyperparameters
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- llama_cpp_client/client.py
+++ llama_cpp_client/client.py
@@ -157,22 +157,90 @@ def get_arguments() -> argparse.Namespace:
         "--base-url",
         type=str,
         default="http://127.0.0.1",
-        help="The servers url",
+        help="The servers url (default: http://127.0.0.1)",
     )
     parser.add_argument(
         "-p",
         "--port",
         type=str,
         default="8080",
-        help="The servers port",
+        help="The servers port (default: 8080)",
+    )
+    parser.add_argument(
+        "--top-k",
+        type=int,
+        default=50,
+        help="Limit output tokens to top-k most likely (default: 50)",
+    )
+    parser.add_argument(
+        "--top-p",
+        type=float,
+        default=0.9,
+        help="Only consider tokens with prob greater than top-p (default: 0.9)",
+    )
+    parser.add_argument(
+        "--min-p",
+        type=float,
+        default=0.1,
+        help="Minimum token probability (default: 0.1)",
+    )
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        default=0.7,
+        help="Temperature for output randomness (default: 0.7)",
+    )
+    parser.add_argument(
+        "--repeat-penalty",
+        type=float,
+        default=1.0,
+        help="Penalty for repeating tokens (default: 1.0, no effect)",
+    )
+    parser.add_argument(
+        "--n-predict",
+        type=int,
+        default=-1,
+        help="The number of tokens to predict (default: -1, inf)",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=1,
+        help="Initial value for random number generator (default: -1, random seed; set to a specific value for reproducible output)",
+    )
+    parser.add_argument(
+        "--cache-prompt",
+        action="store_false",
+        help="Reuse cached prompts to speed up processing (default: true; set to false to disable)",
+    )
+    parser.add_argument(
+        "--stop",
+        type=str,
+        default="",
+        help="List of stop tokens to ignore (default: empty string; use comma delimited list without spaces).",
     )
     return parser.parse_args()
 
 
 def main():
     args = get_arguments()
+
     llama_cpp_request = LlamaCppRequest(args.base_url, args.port)
-    llama_cpp_api = LlamaCppAPI(llama_cpp_request)
+
+    # set models hyperparameters
+    stop = [token for token in args.stop.split(",") if token]
+    llama_cpp_api = LlamaCppAPI(
+        llama_cpp_request,
+        top_k=args.top_k,
+        top_p=args.top_p,
+        min_p=args.min_p,
+        temperature=args.temperature,
+        repeat_penalty=args.repeat_penalty,
+        n_predict=args.n_predict,
+        seed=args.seed,
+        cache_prompt=args.cache_prompt,
+        stop=stop,
+    )
     llama_cpp_tokenizer = LlamaCppTokenizer(llama_cpp_request)
     llama_cpp_history = LlamaCppHistory(args.session_name, args.system_message)
     llama_cpp_client = LlamaCppClient(
