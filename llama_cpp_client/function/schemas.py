9315a10c1b9596baabdf9d5eb6af6a6e587fc2ed
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-04-24 01:09:33 -0400

    feat: Add JSON schema for reading files

--- llama_cpp_client/function/schemas.py
+++ llama_cpp_client/function/schemas.py
@@ -1,3 +1,7 @@
+"""
+Module: llama_cpp_client.function.schemas
+"""
+
 GET_CURRENT_WEATHER = {
     "name": "get_current_weather",
     "description": "Get the current weather in a given location",
@@ -14,6 +18,30 @@ GET_CURRENT_WEATHER = {
     },
 }
 
+READ_FILE = {
+    "name": "read_file",
+    "description": "Reads the contents of a file and returns a substring based on optional line numbers.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "filepath": {
+                "type": "string",
+                "description": "The required file path to read from.",
+            },
+            "start_line": {
+                "type": "integer",
+                "description": "Optional starting line number to extract from. Default is 0.",
+            },
+            "end_line": {
+                "type": "integer",
+                "description": "Optional ending line number to extract up to. If not provided or set to null, it defaults to the last line in the file.",
+            },
+        },
+        "required": ["filepath"],
+    },
+}
+
 BUILTIN_FUNCTION_SCHEMAS = [
     GET_CURRENT_WEATHER,
+    READ_FILE,
 ]
