9f35ee6e9ad8a06d1f9fb4f6470eb2e7942c525a
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-09-15 17:55:18 -0400

    api(patch): Add monkey patch for handling bug with rendering console input

--- llama_cpp_client/api.py
+++ llama_cpp_client/api.py
@@ -1,11 +1,16 @@
 """
+Copyright © 2023 Austin Berrio
+
 Module: llama_cpp_client.api
 """
 
+import html
+import logging
 from typing import Any, Dict, List
 
 import requests
 
+from llama_cpp_client.logger import get_default_logger
 from llama_cpp_client.request import LlamaCppRequest
 
 
@@ -49,16 +54,23 @@ class LlamaCppAPI:
         # Update self.data with any additional parameters provided via kwargs
         self.data.update(kwargs)
 
+        # Debugging is disabled by default
+        log_level = kwargs.get("log_level", logging.INFO)
+        self.logger = get_default_logger("LlamaCppAPI", level=log_level)
+
     @property
     def health(self) -> Dict[str, Any]:
         try:
+            self.logger.debug("Fetching health status")
             return self.request.get("/health")
         except requests.exceptions.ConnectionError as e:
+            self.logger.error(f"ConnectionError: {e}")
             return {"status": "error", "message": e}
 
     @property
     def slots(self) -> List[Dict[str, Any]]:
         """Get the current slots processing state."""
+        self.logger.debug("Fetching slot states")
         return self.request.get("/slots")
 
     def get_ctx_size(self, slot: int = 0) -> int:
@@ -77,12 +89,14 @@ class LlamaCppAPI:
 
     def completion(self, prompt: str) -> Any:
         """Get a prediction given a prompt"""
+        self.logger.debug(f"Sending completion request with prompt: {prompt}")
         endpoint = "/completion"
         self.data["prompt"] = prompt
+        self.logger.debug(f"Completion request data: {self.data}")
 
         if self.data.get("stream"):
             return self.request.stream(endpoint=endpoint, data=self.data)
-        if not self.data.get("stream"):
+        else:
             return self.request.post(endpoint=endpoint, data=self.data)
 
     def chat_completion(self, messages: List[Dict[str, str]]) -> Any:
@@ -95,15 +109,27 @@ class LlamaCppAPI:
         if not self.data.get("stream"):
             return self.request.post(endpoint=endpoint, data=self.data)
 
+    @staticmethod
+    def sanitize(text: str) -> str:
+        """Escape special symbols from a given body of text."""
+        # NOTE: This is a monkey patch and requires refining.
+        body = ""
+        for symbol in text:
+            if symbol == "[":
+                symbol = "\\["
+            if symbol == "]":
+                symbol = "\\]"
+            body += symbol
+        return html.escape(body)
+
 
 if __name__ == "__main__":
-    import json  # Pretty print server response
     import sys  # Allow streaming to stdout
 
     from rich import print  # Decorate output
 
     # Create an instance of LlamaCppAPI
-    llama_api = LlamaCppAPI(n_predict=45)
+    llama_api = LlamaCppAPI(n_predict=45, log_level=logging.DEBUG)
 
     # Example: Get health status of the Llama.cpp server
     health_status = llama_api.health
@@ -111,16 +137,24 @@ if __name__ == "__main__":
 
     # Example: Get slots processing state
     slots_state = llama_api.slots
-    print("Slots State:", json.dumps(slots_state, indent=2))
+    # using json.dumps() seems to be causing conflicts with rich.print()
+    print("Slots State:", slots_state)  # this works fine
 
     # Example: Get model file path for a specific slot
     slot_index = 0
     model_path = llama_api.get_model(slot=slot_index)
     print(f"Model Path for Slot {slot_index}: {model_path}")
 
+    assert llama_api.sanitize("[INST] Test [/INST]") == "\\[INST\\] Test \\[/INST\\]"
+    assert (
+        llama_api.sanitize("This is [example] text.") == "This is \\[example\\] text."
+    )
+    assert llama_api.sanitize("No brackets here!") == "No brackets here!"
+
     # Example: Get prompt for a specific slot
     prompt = llama_api.get_prompt(slot=slot_index)
-    print(f"Prompt for Slot {slot_index}:", prompt)
+    prompt = llama_api.sanitize(prompt)
+    print(f"Prompt for Slot {slot_index}: {prompt}")
 
     # Example: Generate prediction given a prompt
     prompt = "Once upon a time"
