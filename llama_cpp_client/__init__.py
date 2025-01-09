0d3744c9910f504a96350e2f8efecc34100174dd
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-09 00:58:39 -0500

    llama_cpp(chore): Bump revision and add description

--- llama_cpp_client/__init__.py
+++ llama_cpp_client/__init__.py
@@ -1,8 +1,10 @@
 """
 Module: llama_cpp_client
+
+A client facing library for interacting with the llama.cpp REST API.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __name__ = "llama-cpp-client"
 __agent__ = f"teleprint-me/{__name__}"
 __source__ = f"https://github.com/{__agent__}"
